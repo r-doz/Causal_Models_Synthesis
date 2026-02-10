45b10cbbdc10ff7dfab41d44dc8732ee844c62b0
r-doz <romina.ma95@gmail.com>
2026-02-10 09:51:45 +0100

    new grammar and experiments

--- src/algorithm/parameters.py
+++ src/algorithm/parameters.py
@@ -13,19 +13,19 @@ params = {
 
     # Evolutionary Parameters
     'POPULATION_SIZE': 80,
-    'GENERATIONS': 300,
+    'GENERATIONS': 400,
     'HILL_CLIMBING_HISTORY': 1000,
     'SCHC_COUNT_METHOD': "count_all", 
 
     # Set optional experiment name
-    'EXPERIMENT_NAME': 'common_cause_new_grammar', # name of the folder
-    'PROGRAM_NAME': 'common_cause',
+    'EXPERIMENT_NAME': 'diamond_400', # name of the folder
+    'PROGRAM_NAME': 'diamond',
     'INTERVENTIONAL_FITNESS': True,
     'NUM_INTERVENTIONS': 10,
-    'SAVE_STRUCTURES': True,
+    'SAVE_STRUCTURES': False,
     # Set default number of runs to be done.
     # ONLY USED WITH EXPERIMENT MANAGER.
-    'RUNS': 1,
+    'RUNS': 5,
 
     # Class of problem
     #'FITNESS_FUNCTION': "supervised_learning.regression",
@@ -38,7 +38,7 @@ params = {
 
     # Set grammar file
     #'GRAMMAR_FILE': "soga_holes.pybnf",
-    'GRAMMAR_FILE': "causal_SCM_3vars.pybnf",
+    'GRAMMAR_FILE': "causal_SCM_5vars.pybnf",
 
     # Set the number of depths permutations are calculated for
     # (starting from the minimum path of the grammar).
