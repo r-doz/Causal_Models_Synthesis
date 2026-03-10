7271e44d08590ce2582c9f3a545281fb65da1161
r-doz <romina.ma95@gmail.com>
2026-03-10 17:28:40 +0100

    experiments

--- src/algorithm/parameters.py
+++ src/algorithm/parameters.py
@@ -18,14 +18,14 @@ params = {
     'SCHC_COUNT_METHOD': "count_all", 
 
     # Set optional experiment name
-    'EXPERIMENT_NAME': 'chain_no_interventions', # name of the folder
+    'EXPERIMENT_NAME': 'chain_no_if', # name of the folder
     'PROGRAM_NAME': 'chain', # name of the program being synthesised
-    'INTERVENTIONAL_FITNESS': False,
+    'INTERVENTIONAL_FITNESS': True,
     'NUM_INTERVENTIONS': 10,
     'SAVE_STRUCTURES': True,
     # Set default number of runs to be done.
     # ONLY USED WITH EXPERIMENT MANAGER.
-    'RUNS': 3,
+    'RUNS': 2,
 
     # Class of problem
     #'FITNESS_FUNCTION': "supervised_learning.regression",
